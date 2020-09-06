cc31f83a32e5c661a633b4bf088eec160f7222b3
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2020-09-06 11:32:17 +0100

    Implemented createplaylist handler

---
+++ handler/createplaylist.go
@@ -0,0 +1,38 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/collabyt/Backend/database"
+	"github.com/collabyt/Backend/model"
+)
+
+// CreatePlaylist :
+// Insert a new playlist in the database based on the data delivered in JSON
+// format.
+// TODO CreatePlaylist has not been implemented.
+func CreatePlaylist(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var playlist model.Playlist
+	err := json.NewDecoder(r.Body).Decode(&playlist)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		errRet, _ := json.Marshal(
+			model.Error{Description: err.Error()},
+		)
+		w.Write(errRet)
+		return
+	}
+	playlist, err = model.CreatePlaylist(database.DB, playlist)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		errRet, _ := json.Marshal(
+			model.Error{Description: err.Error()},
+		)
+		w.Write(errRet)
+		return
+	}
+	http.Redirect(w, r, "/api/v1/playlist", http.StatusSeeOther)
+	return
+}
