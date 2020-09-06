354bcc94c03d9b02dada4678d464ba0b8ebac679
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2020-09-06 11:31:33 +0100

    created createvideo handler

---
+++ handler/createVideo.go
@@ -0,0 +1,10 @@
+package handler
+
+import "net/http"
+
+// CreateVideo :
+// Add to a playlist a new video.
+// TODO Video handler has not been implemented.
+func CreateVideo(w http.ResponseWriter, r *http.Request) {
+
+}
