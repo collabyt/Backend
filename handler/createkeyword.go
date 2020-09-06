8780f12bc548a6b2414091392e2e3711d9625c67
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2020-09-06 11:31:09 +0100

    Separated getkeyword and createkeyword in handlers

---
+++ handler/createkeyword.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/collabyt/Backend/database"
+	"github.com/collabyt/Backend/model"
+)
+
+// CreateKeyword :
+// insert a new keyword to the database, if it already exists, returns the
+// existing one.
+func CreateKeyword(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var word model.Keyword
+	err := json.NewDecoder(r.Body).Decode(&word)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		errRet, _ := json.Marshal(
+			model.Error{Description: err.Error()},
+		)
+		w.Write(errRet)
+		return
+	}
+	word, err = model.CreateKeyword(database.DB, word.Word)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		errRet, _ := json.Marshal(
+			model.Error{Description: err.Error()},
+		)
+		w.Write(errRet)
+		return
+	}
+	jsonResponse, _ := json.Marshal(word)
+	w.Write(jsonResponse)
+}
---
+++ handler/getkeywords.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/collabyt/Backend/database"
+	"github.com/collabyt/Backend/model"
+)
+
+// GetKeywords :
+// Return a list of (max 10) keywords to be used as part of a new playlist
+func GetKeywords(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	addressParams := r.URL.Query()
+	if len(addressParams["likewise"]) < 1 || len(addressParams["likewise"][0]) < 2 {
+		w.WriteHeader(http.StatusBadRequest)
+		errRet, _ := json.Marshal(
+			model.Error{Description: "likewise string with at least two characters is mandatory"},
+		)
+		w.Write(errRet)
+		return
+	}
+	wordList, err := model.GetKeywordsByPartialWord(database.DB, addressParams["likewise"][0])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		errRet, _ := json.Marshal(model.Error{Description: err.Error()})
+		w.Write(errRet)
+		return
+	}
+	jsonResponse, _ := json.Marshal(wordList)
+	w.Write(jsonResponse)
+}
