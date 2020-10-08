beff0d7ba5cc5f46812221e2dfb0b880e7bc38b7
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2020-10-08 23:58:58 +0100

    Removed Keywords type in favor of just using a slice of Keyword. Also modified methods that used to use this deprecated type.

--- model/keywords.go
+++ model/keywords.go
@@ -4,15 +4,9 @@ import (
 	"database/sql"
 )
 
-// Keywords :
-// represent a list of keyword objects with their respective ID's.
-type Keywords struct {
-	Words []Keyword `json:"keywords"`
-}
-
 // GetKeywordsByPartialWord :
 // returns a list of keyword with a maximum amount of 10.
-func GetKeywordsByPartialWord(db *sql.DB, partWord string) (Keywords, error) {
+func GetKeywordsByPartialWord(db *sql.DB, partWord string) ([]Keyword, error) {
 	rows, err := db.Query(
 		`SELECT id, word FROM keyword 
 		WHERE  word LIKE $1 
@@ -21,15 +15,15 @@ func GetKeywordsByPartialWord(db *sql.DB, partWord string) (Keywords, error) {
 		partWord+"%",
 	)
 	if err != nil {
-		return Keywords{}, err
+		return []Keyword{}, err
 	}
 	var (
-		ks Keywords
+		ks []Keyword
 		k  Keyword
 	)
 	for rows.Next() {
 		rows.Scan(&k.ID, &k.Word)
-		ks.Words = append(ks.Words, k)
+		ks = append(ks, k)
 	}
 	return ks, nil
 }
@@ -37,7 +31,7 @@ func GetKeywordsByPartialWord(db *sql.DB, partWord string) (Keywords, error) {
 // GetKeywordsByID :
 // Given a slice of id's, this will return a list a keywords with all keyword
 // from such slice.s
-func GetKeywordsByID(db *sql.DB, ids []int) (Keywords, error) {
+func GetKeywordsByID(db *sql.DB, ids []int) ([]Keyword, error) {
 	var formattedIDsList string
 	for _, id := range ids {
 		if formattedIDsList != "" {
@@ -52,15 +46,42 @@ func GetKeywordsByID(db *sql.DB, ids []int) (Keywords, error) {
 		formattedIDsList,
 	)
 	if err != nil {
-		return Keywords{}, err
+		return []Keyword{}, err
 	}
-	var (
-		k  Keyword
-		ks Keywords
-	)
+	var ks []Keyword
 	for rows.Next() {
+		var k Keyword
 		rows.Scan(&k)
-		ks.Words = append(ks.Words, k)
+		ks = append(ks, k)
+	}
+	return ks, nil
+}
+
+// GetKeywordsByPlaylistID :
+// Get all keywords that are associated with a given Playlist
+func GetKeywordsByPlaylistID(db *sql.DB, playlistID int) ([]Keyword, error) {
+	kRows, err := db.Query(
+		`SELECT 
+			k.id,
+			k.word
+		FROM 
+			playlist_keyword AS pk
+		INNER JOIN
+			keyword AS k
+		ON
+			pk.keyword_id = k.id
+		WHERE
+			pk.playlist_id = $1;`,
+		playlistID,
+	)
+	if err != nil {
+		return []Keyword{}, err
+	}
+	var ks []Keyword
+	for kRows.Next() {
+		var k Keyword
+		kRows.Scan(&k.ID, &k.Word)
+		ks = append(ks, k)
 	}
 	return ks, nil
 }
