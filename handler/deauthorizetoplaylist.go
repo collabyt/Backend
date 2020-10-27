892c00b76614f0944df1f87116edbe28822bff82
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2020-10-27 22:29:09 +0000

    Implemented endpoint to erase authentication in a playlist

--- handler/deauthorizetoplaylist.go
+++ handler/deauthorizetoplaylist.go
@@ -1,16 +1,29 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/collabyt/Backend/database"
+	"github.com/collabyt/Backend/model"
 )
 
 // DeauthorizeToPlaylist set the cookie related to the given playlist to expire
 func DeauthorizeToPlaylist(w http.ResponseWriter, r *http.Request) {
-	publicID, err := fetchVars(r, "PublicID")
+	w.Header().Set("Content-Type", "application/json")
+	playlistID, err := fetchVars(r, "PublicID")
 	if err != nil {
 		errorStdTreatment(err, w, http.StatusBadRequest)
+		return
 	}
-	fmt.Println(publicID)
-	// NOT FINISHED!
+	cook, err := r.Cookie(playlistID)
+	if err != nil {
+		return
+	}
+	cook.MaxAge = -1
+	http.SetCookie(w, cook)
+	err = model.DeleteSessionBySessionID(database.DB, cook.Value)
+	if err != nil {
+		errorStdTreatment(err, w, http.StatusInternalServerError)
+		return
+	}
 }
