edb1100a498240a013cfc282d4482f912cffd10b
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2020-11-10 22:42:42 +0000

    added todo's

--- database/config.go
+++ database/config.go
@@ -1,12 +1,12 @@
 package database
 
 var (
-	dbHost     = "localhost"
-	dbUser     = "postgres"
-	dbPassword = "postgres"
-	dbDatabase = "collabyt"
-	dbPort     = "5432"
+	dbHost     = "localhost" // TODO: Change to environment variabe
+	dbUser     = "postgres"  // TODO: Change to environment variabe
+	dbPassword = "postgres"  // TODO: Change to environment variabe
+	dbDatabase = "collabyt"  // TODO: Change to environment variabe
+	dbPort     = "5432"      // TODO: Change to environment variabe
 
-	dbSsl    = "disable"
-	dbSource = "postgres"
+	dbSsl    = "disable"  // TODO: Change to environment variabe
+	dbSource = "postgres" // TODO: Change to environment variabe
 )
